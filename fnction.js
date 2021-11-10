b7cbaf778469c83976599e4692f73a630419cfac
daknya_bam <daknyabam18@gmil.com>
2021-11-10 22:40:22 +0530

    change

--- fnction.js
+++ fnction.js
@@ -1,19 +1,41 @@
-const makerandomcolor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-      return `rgb(${r},${g},${b} )`;
-}
+//const makerandomcolor = () => {
+ //   const r = Math.floor(Math.random() * 255);
+ //   const g = Math.floor(Math.random() * 255);
+//    const b = Math.floor(Math.random() * 255);
+//      return `rgb(${r},${g},${b} )`;
+//}
+
+
+
 const buttons = document.querySelectorAll('.self');
 for (let button of buttons){
     button.addEventListener('click',  colorize);
 }
 function colorize(){
-    this.style.backgroundColor = makerandomcolor();
-    this.style.color = makerandomcolor();
+    this.style.backgroundColor = 'black';
+   
 }
 const btn = document.querySelector('#body');
 const body =document.querySelector('.body');
-btn.addEventListener('click',()=>{
-    document.body.style.backgroundColor= makerandomcolor();
+
+
+const black = document.querySelector('#black');
+const erase = document.querySelector('#erase');
+const green = document.querySelector('#green');
+const red = document.querySelector('#red');
+
+
+black.addEventListener('click',function() {
+    console.log('clicked');
+});
+
+
+erase.addEventListener('click',function() {
+    console.log('clicked');
+});
+red.addEventListener('click',function() {
+    console.log('clicked');
+});
+green.addEventListener('click',function() {
+    console.log('clicked');
 });
